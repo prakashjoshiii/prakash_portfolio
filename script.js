18f3ffcef33b87dfd0f1023918ad158db8c3bb48
prakashjoshiii <prakashjoshi2078@gmail.com>
2025-04-24 22:33:26 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll for nav links
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
